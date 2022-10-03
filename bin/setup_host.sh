bad149408e62208929a9a8798abad8f20cbe95a7
heycn <heycn@foxmail.com>
2022-10-03 16:52:15 +0800

    部署到宿主机

---
+++ bin/setup_host.sh
@@ -0,0 +1,14 @@
+DB_PASSWORD=123456
+container_name=mangosteen-prod-1
+
+version=$(cat mangosteen_deploy/version)
+
+echo 'docker build ...'
+docker build mangosteen_deploy -t mangosteen:$version
+if [ "$(docker ps -aq -f name=^mangosteen-prod-1$)" ]; then
+  echo 'docker rm ...'
+  docker rm -f $container_name
+fi
+echo 'docker run ...'
+docker run -d -p 3000:3000 --network=network1 -e DB_PASSWORD=$DB_PASSWORD --name=$container_name mangosteen:$version
+echo 'DONE!'
