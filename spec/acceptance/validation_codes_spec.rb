d5b2989be39722c69d99aa0ddcb28be396f7916d
heycn <heycn@foxmail.com>
2023-03-19 19:23:14 +0800

    引入 rspec_api_documentation gem

---
+++ spec/acceptance/validation_codes_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+require 'rspec_api_documentation/dsl'
+
+resource "验证码" do
+  post "/api/v1/validation_codes" do
+    parameter :email, type: :string
+    let(:email) { '1@qq.com' }
+    example "请求发送验证码" do
+      do_request
+      expect(status).to eq 200
+      expect(response_body).to eq ' '
+    end
+  end
+end
