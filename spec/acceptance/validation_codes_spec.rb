f53f23c7f4d0efe7df7f934ec1a2b06047849a83
heycn <heycn@foxmail.com>
2023-03-19 21:46:19 +0800

    测试邮件被成功发送

--- spec/acceptance/validation_codes_spec.rb
+++ spec/acceptance/validation_codes_spec.rb
@@ -4,8 +4,9 @@ require 'rspec_api_documentation/dsl'
 resource "验证码" do
   post "/api/v1/validation_codes" do
     parameter :email, type: :string
-    let(:email) { '1@qq.com' }
+    let(:email) { 'heycn@foxmail.com' }
     example "请求发送验证码" do
+      expect(UserMailer).to receive(:welcome_email).with(email)
       do_request
       expect(status).to eq 200
       expect(response_body).to eq ' '
