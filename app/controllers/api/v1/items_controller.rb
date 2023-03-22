ea456cf758491d40aaefcc78dab9c594a6b157a5
heycn <heycn@foxmail.com>
2023-03-22 23:18:25 +0800

    添加 sign_in 方法

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    items = Item.where({created_at: params[:created_after]..params[:created_before]}).page(params[:page])
+    current_user_id = request.env['current_user_id']
+    return head :unauthorized if current_user_id.nil?
+    items = Item.where({user_id: current_user_id})
+      .where({created_at: params[:created_after]..params[:created_before]})
+      .page(params[:page])
     render json: { resources: items, pager: {
       page: params[:page] || 1,
       per_page: Item.default_per_page,
