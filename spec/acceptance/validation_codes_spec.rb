6b95a6e1fc20cc375c41c35565c95da9a437d799
heycn <heycn@foxmail.com>
2023-03-28 01:07:09 +0800

    diff

--- spec/acceptance/validation_codes_spec.rb
+++ spec/acceptance/validation_codes_spec.rb
@@ -4,7 +4,7 @@ require 'rspec_api_documentation/dsl'
 resource "验证码" do
   post "/api/v1/validation_codes" do
     parameter :email, type: :string
-    let(:email) { 'heycn@foxmail.com' }
+    let(:email) { '1@qq.com' }
     example "请求发送验证码" do
       expect(UserMailer).to receive(:welcome_email).with(email)
       do_request
