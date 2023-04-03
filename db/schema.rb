ffd60f5cf9afc88bfe9bee1c8b1e8bf1b95a6851
heycn <heycn@foxmail.com>
2023-04-03 23:58:08 +0800

    删除 items 时只会更新 deleted_at

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_03_154633) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_03_155503) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_03_154633) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "kind", null: false
+    t.datetime "deleted_at"
   end
 
   create_table "tags", force: :cascade do |t|
