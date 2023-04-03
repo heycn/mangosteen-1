10999a0cc164b3eb99c57e245fc64fd3158fc3b1
heycn <heycn@foxmail.com>
2023-04-03 23:36:16 +0800

    增加 /items/balance 接口，获取一段时间范围内的余额

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,34 +1,55 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    current_user_id = request.env['current_user_id']
+    current_user_id = request.env["current_user_id"]
     return head :unauthorized if current_user_id.nil?
-    items = Item.where({user_id: current_user_id})
-      .where({created_at: params[:created_after]..params[:created_before]})
+    items = Item.where({ user_id: current_user_id })
+      .where({ happen_at: params[:happen_after]..params[:happen_before] })
       .page(params[:page])
     render json: { resources: items, pager: {
       page: params[:page] || 1,
       per_page: Item.default_per_page,
-      count: Item.count
-    }}
+      count: Item.count,
+    } }
   end
+
   def create
-    item = Item.new params.permit(:amount, :happen_at, tag_ids: [] )  
-    item.user_id = request.env['current_user_id']
+    item = Item.new params.permit(:amount, :happen_at, tag_ids: [])
+    item.user_id = request.env["current_user_id"]
     if item.save
-      render json: {resource: item}
+      render json: { resource: item }
     else
-      render json: {errors: item.errors}, status: :unprocessable_entity
+      render json: { errors: item.errors }, status: :unprocessable_entity
     end
   end
+
+  def balance
+    current_user_id = request.env["current_user_id"]
+    return head :unauthorized if current_user_id.nil?
+    items = Item.where({ user_id: current_user_id })
+      .where({ happen_at: params[:happen_after]..params[:happen_before] })
+    income_items = []
+    expenses_items = []
+    items.each {|item|
+      if item.kind === 'income'
+        income_items << item
+      else
+        expenses_items << item
+      end
+    }
+    income = income_items.sum(&:amount)
+    expenses = expenses_items.sum(&:amount)
+    render json: { income: income, expenses: expenses, balance: income - expenses }
+  end
+
   def summary
     hash = Hash.new
     items = Item
-      .where(user_id: request.env['current_user_id'])
+      .where(user_id: request.env["current_user_id"])
       .where(kind: params[:kind])
       .where(happen_at: params[:happened_after]..params[:happened_before])
     items.each do |item|
-      if params[:group_by] == 'happen_at'
-        key = item.happen_at.in_time_zone('Beijing').strftime('%F')
+      if params[:group_by] == "happen_at"
+        key = item.happen_at.in_time_zone("Beijing").strftime("%F")
         hash[key] ||= 0
         hash[key] += item.amount
       else
@@ -40,15 +61,15 @@ class Api::V1::ItemsController < ApplicationController
       end
     end
     groups = hash
-      .map { |key, value| {"#{params[:group_by]}": key, amount: value} }
-    if params[:group_by] == 'happen_at'
+      .map { |key, value| { "#{params[:group_by]}": key, amount: value } }
+    if params[:group_by] == "happen_at"
       groups.sort! { |a, b| a[:happen_at] <=> b[:happen_at] }
-    elsif params[:group_by] == 'tag_id'
+    elsif params[:group_by] == "tag_id"
       groups.sort! { |a, b| b[:amount] <=> a[:amount] }
     end
     render json: {
       groups: groups,
-      total: items.sum(:amount)
+      total: items.sum(:amount),
     }
   end
 end
