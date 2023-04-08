8997bf34ddfb2c3825ad4347db66c3f35dc36d77
heycn <heycn@foxmail.com>
2023-04-08 23:03:04 +0800

    CORS支持任意域名，但不是用*来做

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '120.77.217.225:8080'
+    origins {true} # 相当于 '*'，但比 * 更好用，因为这样写返回的是请求的域名，而不是 '*'
     resource '*',
         methods: [:get, :post, :delete, :patch, :options, :head],
         headers: :any,
